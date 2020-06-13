c50ccd3eb836c2006a9ddb631a59b4570b55e496
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-06-13 16:24:59 -0500

    add rest of intents

--- SkillCode/lambda/customIntents.js
+++ SkillCode/lambda/customIntents.js
@@ -74,6 +74,60 @@ exports.ProgQuizIntentHandler = {
   },
 };
 
+exports.ProgFactIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === "ProgFactIntent"
+    );
+  },
+  handle(handlerInput) {
+    const index = Math.floor(Math.random() * data.PROGFACTS.length);
+    return (
+      handlerInput.responseBuilder
+        .speak(data.PROGFACTS[index].fact)
+        //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+        .getResponse()
+    );
+  },
+};
+
+exports.AdviceIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === "AdviceIntent"
+    );
+  },
+  async handle(handlerInput) {
+    const response = await giveAdvice();
+    return (
+      handlerInput.responseBuilder
+        .speak(response)
+        //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+        .getResponse()
+    );
+  },
+};
+
+exports.ProgQuoteIntentHandler = {
+  canHandle(handlerInput) {
+    return (
+      Alexa.getRequestType(handlerInput.requestEnvelope) === "IntentRequest" &&
+      Alexa.getIntentName(handlerInput.requestEnvelope) === "ProgQuoteIntent"
+    );
+  },
+  async handle(handlerInput) {
+    const response = await getProgrammingQuote();
+    return (
+      handlerInput.responseBuilder
+        .speak(response)
+        //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+        .getResponse()
+    );
+  },
+};
+
 async function getJoke() {
   const { data } = await axios.get(
     "https://official-joke-api.appspot.com/jokes/programming/random"
@@ -83,5 +137,17 @@ async function getJoke() {
 
 async function getInspQuote() {
   const { data } = await axios.get("http://api.quotable.io/random");
-  return data.content + "\n Said by " + data.author;
+  return data.content + " Said by " + data.author;
+}
+
+async function giveAdvice() {
+  const { data } = await axios.get("https://api.adviceslip.com/advice");
+  return data.slip.advice;
+}
+
+async function getProgrammingQuote() {
+  const { data } = await axios.get(
+    "http://quotes.stormconsultancy.co.uk/random.json"
+  );
+  return data.quote + " Said by  " + data.author;
 }
