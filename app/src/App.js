0fc880a9e282f01422ec9fecf702c2d6ff25dfba
SaloniSS <sxs170015@utdallas.edu>
2020-06-12 23:37:03 -0500

    TTS works

--- app/src/App.js
+++ app/src/App.js
@@ -35,6 +35,16 @@ function App() {
     }
   });
 
+  async function textToSpeech(text) {
+    speech.speak({
+      text: text,
+    }).then(() => {
+      console.log("Success !")
+    }).catch(e => {
+      console.error("An error occurred :", e)
+    })
+  }
+
   async function getCommits() {
     const commits = await axios.get(`https://api.github.com/users/${user}/events`);
     setCommits(commits.data);
@@ -43,25 +53,25 @@ function App() {
   async function getJoke() {
     const { data } =  await axios.get("https://official-joke-api.appspot.com/jokes/programming/random");
     console.log(data[0].setup);
-    //await testToSpeech(data[0].setup);
+    await textToSpeech(data[0].setup);
     console.log(data[0].punchline);
-    //await testToSpeech(data[0].punchline);
+    await textToSpeech(data[0].punchline);
   }
 
   async function getProgrammingQuote() {
     const { data } =  await axios.get("http://quotes.stormconsultancy.co.uk/random.json");
     console.log(data.quote);
-    //await testToSpeech(data.quote);
+    await textToSpeech(data.quote);
     console.log(data.author);
-    //await testToSpeech(data.author);
+    await textToSpeech(data.author);
   }
 
   async function getQuote() {
     const { data } =  await axios.get("http://api.quotable.io/random");
     console.log(data.content);
-    //await testToSpeech(data.content);
+    await textToSpeech(data.content);
     console.log(data.author);
-    //await testToSpeech(data.author);
+    await textToSpeech(data.author);
   }
 
   return (
