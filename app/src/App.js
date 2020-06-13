a6ff46cb6c9698c494b346a9c08067edf2fcba62
SaloniSS <sxs170015@utdallas.edu>
2020-06-12 23:42:22 -0500

    Display Text

--- app/src/App.js
+++ app/src/App.js
@@ -8,6 +8,8 @@ const axios = require("axios");
 function App() {
   const [user, setUser] = useState('');
   const [commits, setCommits] = useState([]);
+  const [quoteOrSetup, setQuoteOrSetup] = useState('');
+  const [authorOrPunchline, setAuthorOrPunchline] = useState('');
 
   //Check if speech is available
   const speech = new Speech()
@@ -53,42 +55,40 @@ function App() {
   async function getJoke() {
     const { data } =  await axios.get("https://official-joke-api.appspot.com/jokes/programming/random");
     console.log(data[0].setup);
+    setQuoteOrSetup(data[0].setup);
     await textToSpeech(data[0].setup);
     console.log(data[0].punchline);
+    setAuthorOrPunchline(data[0].punchline);
     await textToSpeech(data[0].punchline);
   }
 
   async function getProgrammingQuote() {
     const { data } =  await axios.get("http://quotes.stormconsultancy.co.uk/random.json");
     console.log(data.quote);
+    setQuoteOrSetup(data.quote);
     await textToSpeech(data.quote);
     console.log(data.author);
+    setAuthorOrPunchline(" - " + data.author);
     await textToSpeech(data.author);
   }
 
   async function getQuote() {
     const { data } =  await axios.get("http://api.quotable.io/random");
     console.log(data.content);
+    setQuoteOrSetup(data.content);
     await textToSpeech(data.content);
     console.log(data.author);
+    setAuthorOrPunchline(" - " + data.author);
     await textToSpeech(data.author);
   }
 
   return (
     <div className="App">
       <header className="App-header">
+        <p> { quoteOrSetup }</p>
+        <p> { authorOrPunchline }</p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> Duckie </p>
         <button onClick={getJoke}>
           Get a Joke
         </button>
