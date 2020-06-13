a6b6c6cf841fdcba5529475f688f95a1ce4d0654
SaloniSS <sxs170015@utdallas.edu>
2020-06-12 21:03:37 -0500

    Get jokes

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const axios = require("axios");
 
 function App() {
+  async function getJoke() {
+    const { data } =  await axios.get("https://official-joke-api.appspot.com/jokes/programming/random");
+    console.log(data[0].setup);
+    //await testToSpeech(data[0].setup);
+    console.log(data[0].punchline);
+    //await testToSpeech(data[0].punchline);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +27,12 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={getJoke}>
+          Get a Joke
+        </button>
+        <button>
+          Get an Inspirational Quote
+        </button>
       </header>
     </div>
   );
