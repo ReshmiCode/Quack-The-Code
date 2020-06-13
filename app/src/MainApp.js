7ffb9776b271cbdac02a9d6ad7a05c296db1cb6a
CuteMustard <55762397+CuteMustard@users.noreply.github.com>
2020-06-13 14:35:43 -0500

    Programming Facts

--- app/src/MainApp.js
+++ app/src/MainApp.js
@@ -116,6 +116,11 @@ function MainApp() {
     setQuoteOrSetup(PROGQUES[index].question);
     await textToSpeech(PROGQUES[index].question);
   }
+  async function getFact() {
+    const index = Math.floor(Math.random() * PROGQUES.length);
+    setQuoteOrSetup(PROGFACTS[index].fact);
+    await textToSpeech(PROGFACTS[index].fact);
+  }
 
   return (
     <div className="App">
@@ -140,6 +145,7 @@ function MainApp() {
         <button onClick={getProgrammingQuote}>Get a Programming Quote</button>
         <button onClick={getQuote}>Get an Inspirational Quote</button>
         <button onClick={getQuestion}>Get a Programming Question</button>
+        <button onClick={getFact}>Get a Programming Fact</button>
       </header>
     </div>
   );
@@ -169,3 +175,42 @@ const PROGQUES = [
     answer: "D",
   },
 ];
+
+const PROGFACTS = [
+  {
+    fact: "The first programmer in the world was a woman. Her name was Ada Lovelace and she worked on an analytical engine back in the 1,800’s." 
+  },
+  {
+    fact: "Recent studies have shown that over 70% of coding jobs are in fields outside of technology." 
+  },
+  {
+    fact: "The first computer virus was created in 1983." 
+  },
+  {
+    fact: "The first computer game was created in 1961." 
+  },
+  {
+    fact: "The word computer bug was inspired by a real bug. It was founded by Grace Hopper in 1947." 
+  },
+  {
+    fact: "Nowadays, there are over 700 different programming languages. All experts recommend for kids to start with a visual editor and a blockly based programming language for them to learn in a smoother and easier way." 
+  },
+  {
+    fact: "The first programming language per sé was called Fortran, and it was created in the ’50s." 
+  },
+  {
+    fact: "Almost any powered with electricity needs to be coded. Can you imagine?" 
+  },
+  {
+    fact: "Since many programming languages share the same structure, it is easy for students to learn a new programming language once they have already mastered one before." 
+  },
+  {
+    fact: "Computers run on binary code, which means that their software is written using only 1s and 0s." 
+  },
+  {
+    fact: "Learning coding has stunning cognitive-related benefits, such as problem-solving, computational thinking, analytical thinking, creative thinking, leadership-related skills, and even teamwork." 
+  },
+  {
+    fact: "In the near future knowing how to code will be as necessary as knowing how to write is today. Yes, regardless of the field or career your students decide for their future." 
+  },
+]
