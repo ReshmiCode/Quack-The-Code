6d52d40d5843d6c0f72cf4c9997dd8d83ec03fec
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-06-13 10:09:00 -0500

    start on getting commits for today

--- app/src/Duckie.js
+++ app/src/Duckie.js
@@ -1,60 +1,78 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Speech from 'speak-tts'
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Speech from "speak-tts";
+import { Link } from "react-router-dom";
 
 const axios = require("axios");
+const _ = require("lodash");
+const moment = require("moment");
 
 function Duckie() {
-  const [user, setUser] = useState('');
+  const [user, setUser] = useState("");
   const [commits, setCommits] = useState([]);
-  const [quoteOrSetup, setQuoteOrSetup] = useState('');
-  const [authorOrPunchline, setAuthorOrPunchline] = useState('');
+  const [quoteOrSetup, setQuoteOrSetup] = useState("");
+  const [authorOrPunchline, setAuthorOrPunchline] = useState("");
 
   //Check if speech is available
-  const speech = new Speech()
-  if(speech.hasBrowserSupport()) {
-    console.log("speech synthesis supported")
+  const speech = new Speech();
+  if (speech.hasBrowserSupport()) {
+    console.log("speech synthesis supported");
   }
   //Get voices list
-  speech.init().then((data) => {
-    console.log("Speech is ready, voices are available", data)
-  }).catch(e => {
-    console.error("An error occured while initializing : ", e)
-  })
+  speech
+    .init()
+    .then((data) => {
+      console.log("Speech is ready, voices are available", data);
+    })
+    .catch((e) => {
+      console.error("An error occured while initializing : ", e);
+    });
   //Initalize speech with voice
   speech.init({
-    'volume': 1,
-    'lang': 'en-US',
-    'rate': 1,
-    'pitch': 1,
-    'voice':'Samantha',
-    'splitSentences': true,
-    'listeners': {
-      'onvoiceschanged': (voices) => {
-        console.log("Event voiceschanged", voices)
-      }
-    }
+    volume: 1,
+    lang: "en-US",
+    rate: 1,
+    pitch: 1,
+    voice: "Samantha",
+    splitSentences: true,
+    listeners: {
+      onvoiceschanged: (voices) => {
+        console.log("Event voiceschanged", voices);
+      },
+    },
   });
 
   async function textToSpeech(text) {
-    speech.speak({
-      text: text,
-    }).then(() => {
-      console.log("Success !")
-    }).catch(e => {
-      console.error("An error occurred :", e)
-    })
+    speech
+      .speak({
+        text: text,
+      })
+      .then(() => {
+        console.log("Success !");
+      })
+      .catch((e) => {
+        console.error("An error occurred :", e);
+      });
   }
 
   async function getCommits() {
-    const commits = await axios.get(`https://api.github.com/users/${user}/events`);
-    setCommits(commits.data);
+    const commits = await axios.get(
+      `https://api.github.com/users/${user}/events`
+    );
+    const push = _.filter(commits.data, { type: "PushEvent" });
+    const today = _.filter(push, (obj) =>
+      moment(obj.created_at).isSame(moment(), "day")
+    );
+    console.log(today);
+
+    setCommits(today);
   }
 
   async function getJoke() {
-    const { data } =  await axios.get("https://official-joke-api.appspot.com/jokes/programming/random");
+    const { data } = await axios.get(
+      "https://official-joke-api.appspot.com/jokes/programming/random"
+    );
     console.log(data[0].setup);
     setQuoteOrSetup(data[0].setup);
     await textToSpeech(data[0].setup);
@@ -64,7 +82,9 @@ function Duckie() {
   }
 
   async function getProgrammingQuote() {
-    const { data } =  await axios.get("http://quotes.stormconsultancy.co.uk/random.json");
+    const { data } = await axios.get(
+      "http://quotes.stormconsultancy.co.uk/random.json"
+    );
     console.log(data.quote);
     setQuoteOrSetup(data.quote);
     await textToSpeech(data.quote);
@@ -74,7 +94,7 @@ function Duckie() {
   }
 
   async function getQuote() {
-    const { data } =  await axios.get("http://api.quotable.io/random");
+    const { data } = await axios.get("http://api.quotable.io/random");
     console.log(data.content);
     setQuoteOrSetup(data.content);
     await textToSpeech(data.content);
@@ -96,28 +116,27 @@ function Duckie() {
   return (
     <div className="App">
       <header className="App-header">
-        <p> { quoteOrSetup }</p>
-        <p> { authorOrPunchline }</p>
+        <p> {quoteOrSetup}</p>
+        <p> {authorOrPunchline}</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p> Duckie </p>
         <form onSubmit={handleSubmit}>
           <label>
-            <input type="text" placeholder="GitHub User" value={user} onChange={handleChange}/>
+            <input
+              type="text"
+              placeholder="GitHub User"
+              value={user}
+              onChange={handleChange}
+            />
           </label>
           <input type="submit" value="Get Commit" />
         </form>
         <p> {commits.length} Commits</p>
-        <button onClick={getJoke}>
-          Get a Joke
-        </button>
-        <button onClick={getProgrammingQuote}>
-          Get a Programming Quote
-        </button>
-        <button onClick={getQuote}>
-          Get an Inspirational Quote
-        </button>
-        <Link to ='/'>
-            <button> View  Website  </button>
+        <button onClick={getJoke}>Get a Joke</button>
+        <button onClick={getProgrammingQuote}>Get a Programming Quote</button>
+        <button onClick={getQuote}>Get an Inspirational Quote</button>
+        <Link to="/">
+          <button> View Website </button>
         </Link>
       </header>
     </div>
